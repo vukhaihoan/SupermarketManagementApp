fee71fd5180a4a0f696144b1e9cc505fdf2eec99
vukhaihoan <funny.khaihoan@gmail.com>
2023-12-29 13:57:07 +0700

    feat: remove utf8mb4_0900_ai_ci

--- src/sma/database/Tables_DDL.sql
+++ src/sma/database/Tables_DDL.sql
@@ -6,7 +6,7 @@ CREATE TABLE `booth_info` (
   `UDATED_DATE` timestamp NULL DEFAULT NULL,
   `CREATED_DATE` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`BOOTH_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 
 -- supermarket.campaign definition
@@ -22,7 +22,7 @@ CREATE TABLE `campaign` (
   `UPDATED_DATE` timestamp NULL DEFAULT NULL,
   `CREATED_DATE` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`CAMPAIGN_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 
 
 -- supermarket.categories definition
@@ -32,7 +32,7 @@ CREATE TABLE `categories` (
   `UPDATED_DATE` timestamp NULL DEFAULT NULL,
   `CREATED_DATE` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`CATEGORY`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 -- supermarket.customer_level definition
@@ -40,7 +40,7 @@ CREATE TABLE `categories` (
 CREATE TABLE `customer_level` (
   `LEVEL` varchar(100) NOT NULL,
   PRIMARY KEY (`LEVEL`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 -- supermarket.measurements definition
@@ -50,7 +50,7 @@ CREATE TABLE `measurements` (
   `UPDATED_DATE` timestamp NULL DEFAULT NULL,
   `CREATED_DATE` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`MEASUREMENT`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 -- supermarket.customers definition
@@ -68,7 +68,7 @@ CREATE TABLE `customers` (
   PRIMARY KEY (`CUSTOMER_ID`),
   KEY `customers_FK` (`LEVEL`),
   CONSTRAINT `customers_FK` FOREIGN KEY (`LEVEL`) REFERENCES `customer_level` (`LEVEL`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
 
 
 -- supermarket.items definition
@@ -87,7 +87,7 @@ CREATE TABLE `items` (
   KEY `items_FK` (`MEASUREMENT`),
   CONSTRAINT `items_FK` FOREIGN KEY (`MEASUREMENT`) REFERENCES `measurements` (`MEASUREMENT`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `items_FK_1` FOREIGN KEY (`CATEGORY`) REFERENCES `categories` (`CATEGORY`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
 
 -- supermarket.`user` definition
@@ -103,7 +103,7 @@ CREATE TABLE `user` (
   PRIMARY KEY (`USER_ID`),
   KEY `user_FK` (`BOOTH_ID`),
   CONSTRAINT `user_FK` FOREIGN KEY (`BOOTH_ID`) REFERENCES `booth_info` (`BOOTH_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
 
 
 -- supermarket.customer_invoice definition
@@ -124,7 +124,7 @@ CREATE TABLE `customer_invoice` (
   CONSTRAINT `customer_invoice_FK` FOREIGN KEY (`CUSTOMER_ID`) REFERENCES `customers` (`CUSTOMER_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `customer_invoice_FK_1` FOREIGN KEY (`BOOTH_ID`) REFERENCES `booth_info` (`BOOTH_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `customer_invoice_FK_2` FOREIGN KEY (`USER_ID`) REFERENCES `user` (`USER_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4;
 
 
 -- supermarket.invoice_detail definition
@@ -141,4 +141,4 @@ CREATE TABLE `invoice_detail` (
   KEY `invoice_detail_FK_1` (`ITEM_ID`),
   CONSTRAINT `invoice_detail_FK` FOREIGN KEY (`INVOICE_ID`) REFERENCES `customer_invoice` (`INVOICE_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `invoice_detail_FK_1` FOREIGN KEY (`ITEM_ID`) REFERENCES `items` (`ITEM_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
